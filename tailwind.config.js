fbc6bb6853e525695155c9693319312b8fa4b605
wdog <wdog666@gmail.com>
2022-11-23 14:30:53 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["'Poppins'", "arial", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
